6c45a1b9360f4b3e91f7a2962d8dee73b42427c1
kabir0x17 <kabirj2310@gmail.com>
2022-01-11 01:34:49 +0530

    Update 02_Solution_Print_the_Character_at_a_Given_Index.py

---
+++ 01-Exercises-1-8-Strings-lvl1/02_Solution_Print_the_Character_at_a_Given_Index.py
@@ -0,0 +1,16 @@
+hello = "Hello"
+hello2 = "Hello"
+i = 4
+
+print(hello[i])
+# will print "o"
+
+i = 9
+if i > len(hello):
+    print("i is out of range")
+# this will print "i is out of range"
+
+hello2 = ""
+if hello2 == "":
+    print("Empty String")
+# this will print Empty String
